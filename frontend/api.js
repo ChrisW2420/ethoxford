5ec44ae6aa3b3af70768319ec71bb49611e7e246
Chris Myers <cm1220@ic.ac.uk>
2024-03-10 02:10:57 +0000

    working

--- frontend/api.js
+++ frontend/api.js
@@ -1,95 +1,98 @@
+// const url = "http://10.21.241.136:8000/";
+const url = "http://127.0.0.1:8000/";
+
 // check logged in
 function check_logged_in() {
-  fetch('http://10.21.241.136:8000/users/token/refresh/', {
-    method: 'POST',
-    credentials: 'include',
+  fetch(url + "users/token/refresh/", {
+    method: "POST",
+    credentials: "include",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({'refresh': 's'})
+    body: JSON.stringify({ refresh: "s" }),
   })
-  .then(response => {
-    if (!response.ok) {
+    .then((response) => {
+      if (!response.ok) {
+        window.location.href = "login.html";
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Success:", data);
+      // Handle success, such as redirecting to another page or showing a success message
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // Handle errors, such as showing an error message to the user
       window.location.href = "login.html";
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Success:', data);
-    // Handle success, such as redirecting to another page or showing a success message
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-    // Handle errors, such as showing an error message to the user
-    window.location.href = "login.html";
-  });
+    });
 }
 
 // login
-function login (e) {
+function login(e) {
   e.preventDefault(); // Prevent the default form submission
 
-  const email = document.getElementById('inputEmail').value;
-  const password = document.getElementById('inputPassword').value;
+  const email = document.getElementById("inputEmail").value;
+  const password = document.getElementById("inputPassword").value;
   const data = JSON.stringify({
-      email: email,
-      password: password
-    });
-  fetch('http://10.21.241.136:8000/users/login/', {
-    method: 'POST',
-    credentials: 'include',
+    email: email,
+    password: password,
+  });
+  fetch(url + "users/login/", {
+    method: "POST",
+    credentials: "include",
     headers: {
-      'Content-Type': 'application/json',
-
+      "Content-Type": "application/json",
     },
     body: data,
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok: ' + response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Success:', data);
-    // Handle success, such as redirecting to another page or showing a success message
-    window.location.href = "index.html";
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-    // Handle errors, such as showing an error message to the user
-  });
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok: " + response.status);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Success:", data);
+      // Handle success, such as redirecting to another page or showing a success message
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // Handle errors, such as showing an error message to the user
+    });
 }
 
 // register
 function register(e) {
   e.preventDefault(); // Prevent the default form submission
 
-  const email = document.getElementById('inputEmail').value;
-  const password = document.getElementById('inputPassword').value;
+  const email = document.getElementById("inputEmail").value;
+  const password = document.getElementById("inputPassword").value;
   const data = JSON.stringify({
-      email: email,
-      password: password
-    });
-  fetch('http://10.21.241.136:8000/users/register/', {
-    method: 'POST',
+    email: email,
+    password: password,
+  });
+  fetch(url + "users/register/", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: data,
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok: ' + response.status);
-    }
-    return response.json();
-  .then(data => {
-    console.log('Success:', data);
-    // Handle success, such as redirecting to another page or showing a success message
-    window.location.href = "login.html";
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-    // Handle errors, such as showing an error message to the user
-  });
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok: " + response.status);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Success:", data);
+      // Handle success, such as redirecting to another page or showing a success message
+      window.location.href = "login.html";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // Handle errors, such as showing an error message to the user
+    });
 }
--- frontend/register_wallet.js
+++ frontend/register_wallet.js
@@ -1,122 +1,122 @@
-const {ethers} = require("ethers");
+const { ethers } = require("ethers");
 
 async function connectMetamask() {
-    // Check if Metamask is installed
-    if (typeof window.ethereum !== 'undefined') {
-        try {
-            // Request accounts access
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
-            console.log('Connected to Metamask!');
-        } catch (error) {
-            console.error(error);
-        }
-    } else {
-        console.error('Metamask is not installed');
+  // Check if Metamask is installed
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      // Request accounts access
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Connected to Metamask!");
+    } catch (error) {
+      console.error(error);
     }
+  } else {
+    console.error("Metamask is not installed");
+  }
 }
 // first enter the private key of the user to metamask extension after setting up a connection to it
 async function executeMetamask() {
-    // Check if Metamask is installed
-    // contact address 
-    const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // address of the contract
-    // contract abi
-    const abi = [
+  // Check if Metamask is installed
+  // contact address
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // address of the contract
+  // contract abi
+  const abi = [
+    {
+      inputs: [
         {
-          "inputs": [
-            {
-              "internalType": "string",
-              "name": "_name",
-              "type": "string"
-            },
-            {
-              "internalType": "uint256",
-              "name": "_favoriteNumber",
-              "type": "uint256"
-            }
-          ],
-          "name": "addPerson",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
+          internalType: "string",
+          name: "_name",
+          type: "string",
         },
         {
-          "inputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "name": "nameToFavoriteNumber",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
+          internalType: "uint256",
+          name: "_favoriteNumber",
+          type: "uint256",
+        },
+      ],
+      name: "addPerson",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      name: "nameToFavoriteNumber",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "people",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "favoriteNumber",
+          type: "uint256",
         },
         {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "name": "people",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "favoriteNumber",
-              "type": "uint256"
-            },
-            {
-              "internalType": "string",
-              "name": "name",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
+          internalType: "string",
+          name: "name",
+          type: "string",
         },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "retrieve",
+      outputs: [
         {
-          "inputs": [],
-          "name": "retrieve",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
         },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
         {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "_favoriteNumber",
-              "type": "uint256"
-            }
-          ],
-          "name": "store",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        }
-      ];
+          internalType: "uint256",
+          name: "_favoriteNumber",
+          type: "uint256",
+        },
+      ],
+      name: "store",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ];
 
-        // functions from the contract
-      const provider = new ethers.providers.Web3Provider(window.ethereum); // metamask is our provider, using it as an rpc rl could also be http://127.0.0.1:8545/ but local
-      const signer = provider.getSigner(); // get the signer from the provider connected account on metamask
-      const contract = new ethers.Contract(contractAddress, abi, signer); // create a contract instance, whoever connects to the metamask is the signer
-      await contract.store(42); // call the store function from the contract
-    }   
-module.exports = {
-    connectMetamask,
-    executeMetamask
+  // functions from the contract
+  const provider = new ethers.providers.Web3Provider(window.ethereum); // metamask is our provider, using it as an rpc rl could also be http://127.0.0.1:8545/ but local
+  const signer = provider.getSigner(); // get the signer from the provider connected account on metamask
+  const contract = new ethers.Contract(contractAddress, abi, signer); // create a contract instance, whoever connects to the metamask is the signer
+  await contract.store(42); // call the store function from the contract
 }
+module.exports = {
+  connectMetamask,
+  executeMetamask,
+};
