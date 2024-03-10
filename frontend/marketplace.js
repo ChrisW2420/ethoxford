8026edecf881f6436bf7b47095daae7ace0477b3
Kert Laansalu <kertlaansalu@gmail.com>
2024-03-10 14:45:36 +0000

    marketpalce

---
+++ frontend/marketplace.js
@@ -0,0 +1,26 @@
+document.getElementById('microsoft').addEventListener('click', function() {
+    compname = "Microsoft";
+    price = 20;
+    period = 200;
+    window.location.href = 'register_subscription.html?name='+ compname + '&price=' + price + '&period=' + period;
+  });
+
+document.getElementById('twitch').addEventListener('click', function() {
+    compname = "Twitch";
+    price = 20;
+    period = 200;
+    window.location.href = 'register_subscription.html?name='+ compname + '&price=' + price + '&period=' + period;
+});
+document.getElementById('nordvpn').addEventListener('click', function() {
+    compname = "NordVPN";
+    price = 20;
+    period = 200;
+    window.location.href = 'register_subscription.html?name='+ compname + '&price=' + price + '&period=' + period;
+});
+
+document.getElementById('redcross').addEventListener('click', function() {
+    compname = "Red Cross";
+    price = 20;
+    period = 200;
+    window.location.href = 'register_subscription.html?name='+ compname + '&price=' + price + '&period=' + period;
+});
