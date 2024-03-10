77dfd26ef644490444ed7bfedeafa48c138254ae
sheriff4000 <sheriff4000@gmail.com>
2024-03-10 02:03:57 +0000

    deployer + bits

---
+++ deployer.js
@@ -0,0 +1,57 @@
+const ethers = require('ethers');
+const { Web3 } = require('web3');
+const provider = new Web3('wss://base-sepolia-rpc.publicnode.com');
+const bytecode = require('./bytecode.json');
+const abi = require('./abi.json');
+
+const user_priv_key = '0x4399b2fb3ffa344351b43bc95da54385692241d8925e779258a942fef01d5039';
+
+async function createWallet() {
+    // Create a new Ethereum wallet
+    const wallet = ethers.Wallet.createRandom();
+    // Print the generated address and private key
+    console.log(`Address: ${wallet.address}`);
+    console.log(`Private Key: ${wallet.privateKey}`);
+    return wallet
+}
+// web.eth.sendTransaction({from: web.eth.accounts[0], to: "0x9d2a327b320da739ed6b0da33c3809946cc8cf6a", value: web.toWei(2, 'ether')})
+
+async function sendTrans() {
+    userWallet = provider.eth.wallet.add(user_priv_key);
+    destWallet = createWallet();
+    Web3.eth.sendTransaction({
+        from: userWallet.address,
+        to: destWallet.address,
+        value: Web3.utils.toWei('0.001', 'ether'),
+    });
+}
+
+async function deployContract(currency, amt) {
+    destWallet = createWallet();
+    contract = new provider.eth.Contract(abi);
+    deployer = contract.deploy({
+        data: bytecode,
+        arguments: [destWallet.address],
+    });
+}
+
+sendTrans();
+
+
+// async function deployContract() {
+//     destWallet = createWallet();
+//     const contract = new provider.eth.Contract(abi);
+//     const deployer = contract.deploy({
+//         data: bytecode,
+//         arguments: [destWallet.address, 1000],
+//     });
+
+//     console.log('wallet address:', wallet[0].address);
+//     const txReceipt = await deployer.send({
+//         from: wallet[0].address,
+//     });
+
+//     console.log('Contract address:', txReceipt.options.address);
+// }
+
+// createWallet();
